cd2ff89f75077e2f359439e8d165c111f1c16ead
mxie00 <mxie00@gmail.com>
2024-11-03 22:40:27 -0600

    updated app.py to match file name change of algorithm1.py to classification_algorithm.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from algorithms.algorithm1 import my_algorithm_function
+from Showell.algorithms.classification_algorithm import my_algorithm_function
 from api_clients.database_client import fetch_data_from_db
 from api_clients.external_api_client import fetch_api_data
 
