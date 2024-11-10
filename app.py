e0bdc5a9de948886f41cd3c3780aaa12d3e40c92
shohaibb <robinachoudry@gmail.com>
2024-11-09 23:59:11 -0600

    UI upgrades, algorithm codes paused in app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import sys
+import os
 from flask import Flask, render_template
-from Showell.algorithms.classification_algorithm import my_algorithm_function
-from api_clients.database_client import fetch_data_from_db
-from api_clients.external_api_client import fetch_api_data
+
+# Add the current directory to the Python path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# Import functions from your project modules
+# from algorithms.classification_algorithm import my_algorithm_function
+# from api_clients.database_client import fetch_data_from_db
+# from api_clients.external_api_client import fetch_api_data
 
 app = Flask(__name__)
 
@@ -12,6 +19,11 @@ def home():
 @app.route('/browse')
 def browse():
     return render_template('browse.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
