7e9c847558b0d4d28a71e73fd9640df4029d23cd
mxie00 <mxie00@gmail.com>
2024-11-19 19:54:00 -0600

    grabbing only top 1000 games for each category in terms of similarity/ranking, little bit of fixing whitespace in html

--- algorithms/sorted_filtered.py
+++ algorithms/sorted_filtered.py
@@ -44,15 +44,15 @@ def get_sorted_filtered(games, rankings):
                 j += 1
                 
     games_filter_list = []
-    games_filter_list.append([ game for game in games if 'rank_0' in game ]) # china
-    games_filter_list.append([ game for game in games if 'rank_1' in game ]) # egypt
-    games_filter_list.append([ game for game in games if 'rank_2' in game ]) # greece
-    games_filter_list.append([ game for game in games if 'rank_3' in game ]) # japan
-    games_filter_list.append([ game for game in games if 'rank_4' in game ]) # middleeast
-    games_filter_list.append([ game for game in games if 'rank_5' in game ]) # norway
-    games_filter_list.append([ game for game in games if 'rank_6' in game ]) # rome
-    games_filter_list.append([ game for game in games if 'rank_7' in game ]) # lgbtq
-    games_filter_list.append([ game for game in games if 'rank_8' in game ]) # neurodivergent
+    games_filter_list.append(sorted([ game for game in games if 'rank_0' in game ], key=lambda x: x["rank_0"])[:1000]) # china
+    games_filter_list.append(sorted([ game for game in games if 'rank_1' in game ], key=lambda x: x['rank_1'])[:1000]) # egypt
+    games_filter_list.append(sorted([ game for game in games if 'rank_2' in game ], key=lambda x: x['rank_2'])[:1000]) # greece
+    games_filter_list.append(sorted([ game for game in games if 'rank_3' in game ], key=lambda x: x['rank_3'])[:1000]) # japan
+    games_filter_list.append(sorted([ game for game in games if 'rank_4' in game ], key=lambda x: x['rank_4'])[:1000]) # middleeast
+    games_filter_list.append(sorted([ game for game in games if 'rank_5' in game ], key=lambda x: x['rank_5'])[:1000]) # norway
+    games_filter_list.append(sorted([ game for game in games if 'rank_6' in game ], key=lambda x: x['rank_6'])[:1000]) # rome
+    games_filter_list.append(sorted([ game for game in games if 'rank_7' in game ], key=lambda x: x['rank_7'])[:1000]) # lgbtq
+    games_filter_list.append(sorted([ game for game in games if 'rank_8' in game ], key=lambda x: x['rank_8'])[:1000]) # neurodivergent
     
     for i in range(9):
         GAME_NAME = [ game for game in games_filter_list[i] if "name" in game ]
